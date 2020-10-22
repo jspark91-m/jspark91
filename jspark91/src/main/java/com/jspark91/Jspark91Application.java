7e60ae7460e0a5a6b7187a0c76f78a561308f7f1
jspark91 <hhj_tfg@naver.com>
2020-10-22 15:46:07 +0900

    Add files via upload

---
+++ jspark91/src/main/java/com/jspark91/Jspark91Application.java
@@ -0,0 +1,13 @@
+package com.jspark91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jspark91Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jspark91Application.class, args);
+	}
+
+}
